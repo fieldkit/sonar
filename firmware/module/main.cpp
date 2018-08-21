4077d06c1030cd13fd84adc9a297f757a24f9c2b
Jacob Lewallen <jlewallen@gmail.com>
2018-08-21 11:49:48 -0700

    Use new Arduino CMake stuff.

--- firmware/module/main.cpp
+++ firmware/module/main.cpp
@@ -16,12 +16,10 @@ SonarModule::SonarModule(fk::ModuleInfo &info) : Module(bus, info) {
 
 fk::ModuleReadingStatus SonarModule::beginReading(fk::PendingSensorReading &pending) {
     auto DistanceFromWaterBedInMeters = 1.0f;
-    auto started = millis();
     auto value = analogRead(A1);
     auto voltage = value * (3.3f / 1024.0f);
     auto distance = voltage * (1000.0f / 3.2f);
     auto depth = DistanceFromWaterBedInMeters - (distance / 100.0);
-    auto elapsed = millis() - started;
 
     auto readings = pending.readings;
     auto i = 0;
@@ -52,7 +50,7 @@ void setup() {
     firmware_build_set(FIRMWARE_BUILD);
     firmware_compiled_set(DateTime(__DATE__, __TIME__).unixtime());
 
-    loginfof("Module", "Starting (%d free)", fk_free_memory());
+    loginfof("Module", "Starting (%lu free)", fk_free_memory());
 
     fk::SensorInfo sensors[3] = {
         { "distance", "m" },
